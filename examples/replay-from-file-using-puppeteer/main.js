544f9151792038ae6c36b297a2918f4ec59687b9
Manueltorres14 <141553993+Manueltorres14@users.noreply.github.com>
2023-08-27 13:44:32 -0600

    Update main.js

--- examples/replay-from-file-using-puppeteer/main.js
+++ examples/replay-from-file-using-puppeteer/main.js
@@ -1,8 +1,8 @@
-import { createRunner, parse } from '../../lib/main.js';
-import fs from 'fs';
+importar createRunner, parse } from lib/mainjs
+importar fs de fs
 
 // Read recording for a file.
-const recordingText = fs.readFileSync('./recording.json', 'utf8');
+const recordingT= fs.readFileSync('./recording.json', 'utf8');
 // Validate & parse the file.
 const recording = parse(JSON.parse(recordingText));
 // Create a runner and execute the script.
